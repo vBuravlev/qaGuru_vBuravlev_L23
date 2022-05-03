715a6f8b9b8e0449c68010f705cea14adcbc96e4
Vadim Buravlev <masta54@mail.ru>
2022-05-03 08:29:49 +0700

    update 4 commit

--- src/test/java/config/ProjectConfig.java
+++ src/test/java/config/ProjectConfig.java
@@ -4,6 +4,7 @@ import org.aeonbits.owner.Config;
 
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
+        "system.properties",
         "classpath:config/local.properties",
         "classpath:config/remote.properties"
 })
